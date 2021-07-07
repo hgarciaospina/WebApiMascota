ed55300fc90eca5bad0eab5dbdf2802aa5c9943e
HENRY GARCIA OSPINA <henrygarcia0309@gmail.com>
2021-07-07 11:29:58 -0500

    Migration TableMascota and layered configuration

---
+++ DataAccess/MascotaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiMascota.Models;
+
+namespace DataAccess
+{
+
+    public class MascotaContext : DbContext
+    {
+        public MascotaContext(DbContextOptions<MascotaContext> options)
+          : base(options)
+        {
+        }
+        public DbSet<Mascota> Mascotas { get; set; }
+    }
+}
